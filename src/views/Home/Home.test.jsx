8aee8529c3648c252fc66304cfd07980bc0ff78d
Pete Seeling <peteseeling@gmail.com>
2022-04-25 17:55:41 -0600

    motto/interest test passing in home.test

--- src/views/Home/Home.test.jsx
+++ src/views/Home/Home.test.jsx
@@ -2,10 +2,21 @@ import { render, screen } from "@testing-library/react";
 import Profile from "../../components/Profile/Profile";
 
 
-test('Should render the user profile', async () => {
+test('Should test that Vonta is displayed', async () => {
 render(<Profile />)
 
-const name = await screen.getByText('Meet');
-
-
+const name = await screen.getByText('Vonta');
 })
+
+test('Should test that the motto is displayed', async () => {
+  render(<Profile />)
+  
+  const motto = await screen.getByText('Res Non Verba');
+  })
+
+  test('Should test that interest heading is displayed', async () => {
+    render(<Profile />)
+    
+    const interestHeading= await screen.getByText('Interests');
+  
+    })
